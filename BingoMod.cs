507edadefdac5e665a6342045adb7a7042feade3
Starwort <tcphone93@gmail.com>
2022-09-07 17:08:16 +0100

    Add project files.

---
+++ BingoMod.cs
@@ -0,0 +1,15 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace BingoMod {
+	public class BingoMod : Mod {
+        internal BoardUI boardUI;
+		public BingoMod() { }
+
+        public override void Load() {
+            if (!Main.dedServ) {
+                //
+            }
+        }
+    }
+}
