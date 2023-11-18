9f251fea05c9cc064e17502ce2907c490aa67f9d
Starwort <tcphone93@gmail.com>
2023-11-18 13:28:22 +0000

    Create IAnimatedObject for generalised object animation, make AssetCycleAnimation use that and seal its overrides

--- AnimationHelpers/AssetCycleAnimation.cs
+++ AnimationHelpers/AssetCycleAnimation.cs
@@ -1,26 +1,18 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria.GameContent;
-using Terraria.ModLoader;
 
 namespace BingoBoardCore.AnimationHelpers {
-    public abstract class AssetCycleAnimation : ModItem {
+    public abstract class AssetCycleAnimation : IAnimatedObject {
         // Placeholder, used to give tML something to load
-        public override string Texture => $"Terraria/Images/CoolDown";
+        public sealed override string Texture => "Terraria/Images/CoolDown";
 
-        public AssetCycleAnimation() {
-            IconAnimationSystem.assetAnimations.Add(this);
-        }
-
-        // Get the textureasset corresponding with what to show this seq
+        // Get the textureasset corresponding with what to show this frame
         // only called when the texture should update, so safe to make stateful decisions
         // (like calling an rng)
         public abstract Asset<Texture2D> getFrame(uint frame);
 
-        // Set the next seq of the animation. The seq parameter corresponds
-        // to the total number of frames displayed so far. Will only be called
-        // when we want a new seq
-        internal void animate(uint frame) {
+        public sealed override void animate(uint frame) {
             TextureAssets.Item[Type] = getFrame(frame);
         }
     }
---
+++ AnimationHelpers/IAnimatedObject.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ModLoader;
+
+namespace BingoBoardCore.AnimationHelpers {
+    public abstract class IAnimatedObject : ModItem {
+        public IAnimatedObject() {
+            IconAnimationSystem.animatedObjects.Add(this);
+        }
+
+        // Update the animation. The frame parameter corresponds
+        // to the total number of frames displayed so far. Will only be called
+        // when we want a new frame
+        public abstract void animate(uint frame);
+    }
+}
