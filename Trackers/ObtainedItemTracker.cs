8ba6c5a5c6a41a12f06aebb519658884b05a4797
Starwort <tcphone93@gmail.com>
2024-05-20 19:30:55 +0100

    Create player- and tracker-specific storage for `ObtainedItemTracker`s

--- Trackers/ObtainedItemTracker.cs
+++ Trackers/ObtainedItemTracker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
@@ -42,9 +43,31 @@ namespace BingoBoardCore.Trackers {
             }
         }
 
+        Dictionary<(Type, Type), dynamic?> genericStorage = new();
+
+        internal T storage<T>(Type owner, Func<T> createDefault) {
+            var key = (owner, typeof(T));
+            if (!genericStorage.ContainsKey(key)) {
+                genericStorage.Add(key, createDefault());
+            }
+            // this is fine - can only be null if T is a nullable type
+            // and the type will always match
+            return (T)genericStorage[key]!;
+        }
+
+        internal T storage<T>(Type owner) {
+            // this is fine - can only be null if T is a nullable type
+            // and the type will always match
+            return (T) genericStorage[(owner, typeof(T))]!;
+        }
+
         internal void reset() {
             usedAccs.Clear();
             obtainedItems.Clear();
+            genericStorage.Clear();
+            foreach (var tracker in trackers) {
+                tracker.prepare(Player);
+            }
         }
     }
     internal class CraftAndArmourSetTracker : GlobalItem {
@@ -65,6 +88,44 @@ namespace BingoBoardCore.Trackers {
         public virtual void onCraftItem(Player player, Item item) {
         }
         public virtual void onAnyObtain(Player player, Item item) {
+        }
+        /// <summary>
+        /// Prepare the tracker to track its goal.
+        /// </summary>
+        /// <param name="player"></param>
+        public virtual void prepare(Player player) {
+        }
+
+        /// <summary>
+        /// Player-specific storage, accessible only to this tracker. Missing values will be created using <paramref name="createDefault"/>.
+        /// <br/>
+        /// Only one value can be stored per type.
+        /// <br/>
+        /// Will be cleared <em>before</em> <see cref="prepare(Player)"/> is called.
+        /// <br/>
+        /// <strong>Do not</strong> attempt to store value types here - they will not be updated.
+        /// </summary>
+        /// <typeparam name="T">The type of storage to use</typeparam>
+        /// <param name="player">The <see cref="Player"/> owning the value</param>
+        /// <param name="createDefault">A factory to create the value if it doesn't exist yet (always true on first call)</param>
+        /// <returns>The storage value</returns>
+        private T storage<T>(Player player, Func<T> createDefault) {
+            return player.GetModPlayer<ObtainedItemTrackerImpl>().storage(this.GetType(), createDefault);
+        }
+        /// <summary>
+        /// Player-specific storage, specifically for this tracker. Missing values will cause a <see cref="KeyNotFoundException"/>.
+        /// <br/>
+        /// Only one value can be stored per type.
+        /// <br/>
+        /// Will be cleared <em>before</em> <see cref="prepare(Player)"/> is called.
+        /// <br/>
+        /// <strong>Do not</strong> attempt to store value types here - they will not be updated.
+        /// </summary>
+        /// <typeparam name="T">The type of storage to use</typeparam>
+        /// <param name="player">The <see cref="Player"/> owning the value</param>
+        /// <returns>The storage value</returns>
+        private T storage<T>(Player player) {
+            return player.GetModPlayer<ObtainedItemTrackerImpl>().storage<T>(this.GetType());
         }
 
         protected sealed override void Register() {
