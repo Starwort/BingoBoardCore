5e6f3423c0b3203884b7509a6f7b8df53864bfa2
Starwort <tcphone93@gmail.com>
2024-05-21 10:48:42 +0100

    Change ObtainedItemTracker to use PlayerTracker Eliminate need for custom storage mechanism by using automatic ModPlayer storage

--- Trackers/ObtainedItemTracker.cs
+++ Trackers/ObtainedItemTracker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.DataStructures;
@@ -7,6 +7,14 @@ using Terraria.ModLoader;
 namespace BingoBoardCore.Trackers {
     internal class ObtainedItemTrackerImpl : ModPlayer {
         internal static readonly List<ObtainedItemTracker> trackers = new();
+        internal List<ObtainedItemTracker> myTrackers = new();
+
+        public override void Initialize() {
+            myTrackers = (
+                from tracker in trackers
+                select Player.GetModPlayer(tracker)
+            ).ToList();
+        }
 
         internal HashSet<int> usedAccs = new();
         internal void onEquipAccessory(Item acc) {
@@ -14,14 +22,14 @@ namespace BingoBoardCore.Trackers {
                 return;
             }
             usedAccs.Add(acc.type);
-            foreach (var tracker in trackers) {
-                tracker.onEquipAccessory(Player, acc);
+            foreach (var tracker in myTrackers) {
+                tracker.onEquipAccessory(acc);
             }
         }
         internal void onCraftItem(Item itm) {
-            foreach (var tracker in trackers) {
-                tracker.onCraftItem(Player, itm);
-                tracker.onAnyObtain(Player, itm);
+            foreach (var tracker in myTrackers) {
+                tracker.onCraftItem(itm);
+                tracker.onAnyObtain(itm);
             }
         }
         internal HashSet<int> obtainedItems = new();
@@ -30,8 +38,8 @@ namespace BingoBoardCore.Trackers {
                 return;
             }
             obtainedItems.Add(itm.type);
-            foreach (var tracker in trackers) {
-                tracker.onAnyObtain(Player, itm);
+            foreach (var tracker in myTrackers) {
+                tracker.onAnyObtain(itm);
             }
         }
 
@@ -43,30 +51,11 @@ namespace BingoBoardCore.Trackers {
             }
         }
 
-        Dictionary<(Type, Type), dynamic?> genericStorage = new();
-
-        internal T storage<T>(Type owner, Func<T> createDefault) {
-            var key = (owner, typeof(T));
-            if (!genericStorage.ContainsKey(key)) {
-                genericStorage.Add(key, createDefault());
-            }
-            // this is fine - can only be null if T is a nullable type
-            // and the type will always match
-            return (T)genericStorage[key]!;
-        }
-
-        internal T storage<T>(Type owner) {
-            // this is fine - can only be null if T is a nullable type
-            // and the type will always match
-            return (T) genericStorage[(owner, typeof(T))]!;
-        }
-
         internal void reset() {
             usedAccs.Clear();
             obtainedItems.Clear();
-            genericStorage.Clear();
-            foreach (var tracker in trackers) {
-                tracker.prepare(Player);
+            foreach (var tracker in myTrackers) {
+                tracker.prepare();
             }
         }
     }
@@ -82,54 +71,28 @@ namespace BingoBoardCore.Trackers {
         }
     }
 
-    public abstract class ObtainedItemTracker : ModType {
-        public virtual void onEquipAccessory(Player player, Item acc) {
+    public abstract class ObtainedItemTracker : PlayerTracker {
+        public virtual void onEquipAccessory(Item acc) {
         }
-        public virtual void onCraftItem(Player player, Item item) {
+        public virtual void onCraftItem(Item item) {
         }
-        public virtual void onAnyObtain(Player player, Item item) {
+        public virtual void onAnyObtain(Item item) {
         }
         /// <summary>
         /// Prepare the tracker to track its goal.
         /// </summary>
-        /// <param name="player"></param>
-        public virtual void prepare(Player player) {
+        public virtual void prepare() {
         }
 
         /// <summary>
-        /// Player-specific storage, accessible only to this tracker. Missing values will be created using <paramref name="createDefault"/>.
-        /// <br/>
-        /// Only one value can be stored per type.
-        /// <br/>
-        /// Will be cleared <em>before</em> <see cref="prepare(Player)"/> is called.
-        /// <br/>
-        /// <strong>Do not</strong> attempt to store value types here - they will not be updated.
+        /// Replacement for <see cref="Load"/>, which is sealed as it performs important initialisation.
         /// </summary>
-        /// <typeparam name="T">The type of storage to use</typeparam>
-        /// <param name="player">The <see cref="Player"/> owning the value</param>
-        /// <param name="createDefault">A factory to create the value if it doesn't exist yet (always true on first call)</param>
-        /// <returns>The storage value</returns>
-        protected T storage<T>(Player player, Func<T> createDefault) {
-            return player.GetModPlayer<ObtainedItemTrackerImpl>().storage(this.GetType(), createDefault);
-        }
-        /// <summary>
-        /// Player-specific storage, specifically for this tracker. Missing values will cause a <see cref="KeyNotFoundException"/>.
-        /// <br/>
-        /// Only one value can be stored per type.
-        /// <br/>
-        /// Will be cleared <em>before</em> <see cref="prepare(Player)"/> is called.
-        /// <br/>
-        /// <strong>Do not</strong> attempt to store value types here - they will not be updated.
-        /// </summary>
-        /// <typeparam name="T">The type of storage to use</typeparam>
-        /// <param name="player">The <see cref="Player"/> owning the value</param>
-        /// <returns>The storage value</returns>
-        protected T storage<T>(Player player) {
-            return player.GetModPlayer<ObtainedItemTrackerImpl>().storage<T>(this.GetType());
+        public virtual void onLoad() {
         }
 
-        protected sealed override void Register() {
+        public sealed override void Load() {
             ObtainedItemTrackerImpl.trackers.Add(this);
+            onLoad();
         }
     }
 }
