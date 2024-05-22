8cf4af680e47d9997fe930e317ab9dc9f6fac9a1
Starwort <tcphone93@gmail.com>
2024-05-22 22:01:20 +0100

    Add NpcTracker as a thin layer over GlobalNPC

---
+++ Trackers/NpcTracker.cs
@@ -0,0 +1,7 @@
+﻿using Terraria.ModLoader;
+
+namespace BingoBoardCore.Trackers {
+    public abstract class NpcTracker : GlobalNPC {
+        public override string Name => this.GetType().AssemblyQualifiedName ?? this.GetType().Name;
+    }
+}
